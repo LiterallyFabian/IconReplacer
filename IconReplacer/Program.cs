4dbfb7bc3c9a229c3bcb128489b7529f8c6b87db
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-03-18 14:42:22 +0100

    Finish program

--- IconReplacer/Program.cs
+++ IconReplacer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 
 namespace IconReplacer
 {
@@ -6,7 +6,42 @@ namespace IconReplacer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "./input";
+            string[] files = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+            string icon = GetIcon();
+            
+            Directory.CreateDirectory("output");
+            foreach (string file in files)
+            {
+                string output =  @".\output" + file.Replace(path, "");
+                Console.WriteLine(output);
+                
+                if (File.Exists(output))
+                {
+                    File.Delete(output);
+                }
+                
+                Directory.CreateDirectory(new FileInfo(output).DirectoryName ?? string.Empty);
+                
+                File.Copy(icon, output);
+            }
+        }
+        
+        private static string GetIcon()
+        {
+            string? path = null;
+            
+            while (path == null)
+            {
+                Console.Write("Enter icon path: ");
+                path = Console.ReadLine()?.Trim('"');
+
+                if (File.Exists(path)) continue;
+                Console.WriteLine("File does not exist");
+                path = null;
+            }
+
+            return path;
         }
     }
 }
